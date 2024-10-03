ac45e0c9a9d6f9056599b7c26bc361d0537836c1
ivylijingru <1084309295@qq.com>
2024-10-03 01:58:00 -0400

    add logging mel spec

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,8 @@ from typing import Any
 import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+import numpy as np
 
 from .basemodel import get_base_model
 from .loss_fn import get_loss_fn
@@ -17,9 +19,10 @@ class ReconstructModel(pl.LightningModule):
         self.optim_cfg = configs["optim"]
         self.model = get_base_model(configs["mlp"])
         self.loss_fn = get_loss_fn()
+        self.example_batch = None
 
     def training_step(self, batch, batch_idx) -> Any:
-        loss_dict = self.common_step(batch)
+        loss_dict, _ = self.common_step(batch)
 
         self.log("lr", self.optimizers().optimizer.param_groups[0]["lr"])
         self.log_dict_prefix(loss_dict, "train")
@@ -29,16 +32,24 @@ class ReconstructModel(pl.LightningModule):
         return loss_dict["loss/total"]
 
     def validation_step(self, batch, batch_idx) -> Any:
-        loss_dict = self.common_step(batch)
-
+        loss_dict, mel_output = self.common_step(batch)
+    
+        if self.example_batch is None and batch_idx == 0:
+            self.example_batch = batch
+            self.example_batch["mel_pred"] = mel_output
         self.log_dict_prefix(loss_dict, "val")
         
         # self.val_metrics.update(logits, torch.round(batch["y"]), batch["y_mask"])
 
         return loss_dict["loss/total"]
 
+    def on_validation_epoch_end(self):
+        if self.example_batch is not None:
+            self.log_image(self.example_batch["mel"][0], "gt")
+            self.log_image(self.example_batch["mel_pred"][0], "pred")
+
     def test_step(self, batch, batch_idx):
-        loss_dict = self.common_step(batch)
+        loss_dict, _ = self.common_step(batch)
 
         self.log_dict_prefix(loss_dict, "test")
 
@@ -59,11 +70,31 @@ class ReconstructModel(pl.LightningModule):
         loss_dict["loss/total"] = total_loss
 
         # TODO: potentially log mel spectrogram here
-        return loss_dict
+        return loss_dict, model_output
 
     def log_dict_prefix(self, d, prefix):
         for k, v in d.items():
             self.log("{}/{}".format(prefix, k), v)
+
+    def log_image(self, mel_spectrogram, note):
+        # 将频谱图转换为 NumPy 数组，以便绘制
+        mel_spectrogram_np = mel_spectrogram.detach().cpu().numpy()
+
+        # 使用 Matplotlib 将梅尔频谱图转换为彩色图像
+        fig, ax = plt.subplots()
+        cax = ax.imshow(mel_spectrogram_np, aspect='auto', cmap='viridis')  # 选择一个彩色 colormap，比如 viridis
+        fig.colorbar(cax)
+
+        # 将 Matplotlib 图像保存到内存中
+        fig.canvas.draw()
+        img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+        # 记录到 TensorBoard
+        self.logger.experiment.add_image('MelSpectrogram_{}'.format(note), img, self.current_epoch, dataformats='HWC')
+
+        # 关闭 Matplotlib 图像以释放内存
+        plt.close(fig)
 
     def configure_optimizers(self) -> Any:
         optimizer_cfg = self.optim_cfg["optimizer"]
--- train.py
+++ train.py
@@ -21,17 +21,15 @@ def train(config):
     datamodule = ReconstructDataModule(**data_cfg)
     model = ReconstructModel(model_cfg)
 
-    """
     callbacks = [
         ModelCheckpoint(**trainer_cfg["checkpoint"]),
         EarlyStopping(**trainer_cfg["early_stopping"])
     ]
-    """
 
     trainer = pl.Trainer(
         **trainer_cfg["args"],
         logger=TensorBoardLogger(**trainer_cfg["logger"]),
-        # callbacks=callbacks
+        callbacks=callbacks
     )
 
     trainer.fit(model, datamodule=datamodule)
